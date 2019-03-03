7456c9d80094b9b9f130bd734f8e3b2e8257c4ad
JAePortfolio <jarena000@citymail.cuny.edu>
2019-03-02 19:01:07 -0500

    Fixed bug in 16x4 SRAM

--- Arena_John/Lab 2/Quartus Files/simulation/qsim/work/@arena_16x4_@s@r@a@m/_primary.vhd
+++ Arena_John/Lab 2/Quartus Files/simulation/qsim/work/@arena_16x4_@s@r@a@m/_primary.vhd
@@ -3,10 +3,10 @@ use verilog.vl_types.all;
 entity Arena_16x4_SRAM is
     port(
         Arena_OUT       : out    vl_logic_vector(3 downto 0);
-        Arena_Address   : in     vl_logic_vector(3 downto 0);
         Arena_WE        : in     vl_logic;
         Arena_CS        : in     vl_logic;
         Arena_DataIn    : in     vl_logic_vector(3 downto 0);
+        Arena_Address   : in     vl_logic_vector(3 downto 0);
         Arena_OE        : in     vl_logic
     );
 end Arena_16x4_SRAM;
