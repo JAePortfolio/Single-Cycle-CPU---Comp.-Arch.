243e324314a8c2df2921c7b43ba84a1190a5be46
JAePortfolio <jarena000@citymail.cuny.edu>
2019-03-05 16:51:41 -0500

    Updated Report

--- Arena_John/Lab 2/Quartus Files/Arena_4to16Decoder/Arena_4to16Decoder.vhd
+++ Arena_John/Lab 2/Quartus Files/Arena_4to16Decoder/Arena_4to16Decoder.vhd
@@ -1,20 +1,22 @@
+-- (First, Last) John Arena - CSC 342/343 - Lab 2 - Spring 2019 Due: 3/6/19
+-- Arena_4to16Decoder.vhd
 library ieee;
 use ieee.std_logic_1164.all;
 
 entity Arena_4to16Decoder is
 	port(
-		Arena_In: in std_logic_vector(3 downto 0);
-		Arena_Dec: out std_logic_vector(15 downto 0)
+		Arena_In: in std_logic_vector(3 downto 0); -- 4 inputs
+		Arena_Dec: out std_logic_vector(15 downto 0) -- 16 outputs
 		);
 end Arena_4to16Decoder;
 
-architecture Arena_Arch_4to16Decoder of Arena_4to16Decoder is
+architecture Arena_Arch_4to16Decoder of Arena_4to16Decoder is -- Architecture to describe functionalitty
 
 begin
 
 with Arena_In select
-	Arena_Dec <= "0000000000000001" when "0000",
-					 "0000000000000010" when "0001",
+	Arena_Dec <= "0000000000000001" when "0000", -- F0
+					 "0000000000000010" when "0001", -- F1...
 					 "0000000000000100" when "0010",
 					 "0000000000001000" when "0011",
 					 "0000000000010000" when "0100",
@@ -28,6 +30,6 @@ with Arena_In select
 					 "0001000000000000" when "1100",
 					 "0010000000000000" when "1101",
 					 "0100000000000000" when "1110",
-					 "1000000000000000" when "1111",
-					 "0000000000000000" when others;
+					 "1000000000000000" when "1111", --...F15
+					 "0000000000000000" when others; 
 end Arena_Arch_4to16Decoder;
