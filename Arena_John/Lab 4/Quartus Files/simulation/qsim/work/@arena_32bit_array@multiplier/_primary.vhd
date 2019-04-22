7287152b5c5be907d14ea6c8e917d2a1751230cd
JAePortfolio <jarena000@citymail.cuny.edu>
2019-04-22 18:41:58 -0400

    Finished 4 bit multiplier

--- Arena_John/Lab 4/Quartus Files/simulation/qsim/work/@arena_32bit_array@multiplier/_primary.vhd
+++ Arena_John/Lab 4/Quartus Files/simulation/qsim/work/@arena_32bit_array@multiplier/_primary.vhd
@@ -2,7 +2,7 @@ library verilog;
 use verilog.vl_types.all;
 entity Arena_32bit_arrayMultiplier is
     port(
-        Arena_out       : out    vl_logic_vector(15 downto 0);
+        Arena_Product_Out: out    vl_logic_vector(7 downto 0);
         Arena_buttonSelect_one: in     vl_logic;
         Arena_buttonSelect_two: in     vl_logic;
         Arena_A_In      : in     vl_logic_vector(15 downto 0);
