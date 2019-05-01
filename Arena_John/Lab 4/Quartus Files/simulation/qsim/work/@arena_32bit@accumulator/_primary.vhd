0770ab902f13c2a0dfb909901f24738182b58adc
JAePortfolio <jarena000@citymail.cuny.edu>
2019-04-30 21:31:56 -0400

    Fixed clock to be active low

--- Arena_John/Lab 4/Quartus Files/simulation/qsim/work/@arena_32bit@accumulator/_primary.vhd
+++ Arena_John/Lab 4/Quartus Files/simulation/qsim/work/@arena_32bit@accumulator/_primary.vhd
@@ -6,11 +6,10 @@ entity Arena_32bitAccumulator is
         Arena_sub_add   : in     vl_logic;
         Arena_Cin_32bit : in     vl_logic;
         Arena_Bin_32bit : in     vl_logic;
+        Arena_reset     : in     vl_logic;
         Arena_clk       : in     vl_logic;
         Arena_A_32bit   : in     vl_logic_vector(31 downto 0);
         Arena_Bout_32bit: out    vl_logic;
-        Arena_Q         : out    vl_logic_vector(31 downto 0);
-        Arena_Sum_32bit : out    vl_logic_vector(31 downto 0);
-        Arena_Sum_Immediate: out    vl_logic_vector(31 downto 0)
+        Arena_AccumOut_32bit: out    vl_logic_vector(31 downto 0)
     );
 end Arena_32bitAccumulator;
--- Arena_John/Lab 4/Quartus Files/simulation/qsim/work/@arena_32bit@accumulator_vlg_sample_tst/_primary.vhd
+++ Arena_John/Lab 4/Quartus Files/simulation/qsim/work/@arena_32bit@accumulator_vlg_sample_tst/_primary.vhd
@@ -6,6 +6,7 @@ entity Arena_32bitAccumulator_vlg_sample_tst is
         Arena_Bin_32bit : in     vl_logic;
         Arena_Cin_32bit : in     vl_logic;
         Arena_clk       : in     vl_logic;
+        Arena_reset     : in     vl_logic;
         Arena_sub_add   : in     vl_logic;
         sampler_tx      : out    vl_logic
     );
